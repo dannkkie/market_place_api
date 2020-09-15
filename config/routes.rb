66d13474d016b68047997c25ab9f20f3cc9dcf3d
Edmund <ekkpodogui@gmail.com>
2020-09-15 22:43:24 +0000

    Adds show action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   #API definition
   namespace :api, defaults: { format: :json } do
-    namespace :V1 do
-    
+    namespace :v1 do
+      resources :users, only: [:show]
     end
   end
 end
